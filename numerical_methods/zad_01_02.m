29b0f5d71a46035004c6f183e7fe07a0c1e7ea86
Mateuszszszszsz <133503123+Mateuszszszszsz@users.noreply.github.com>
2024-03-04 21:39:13 +0100

    Add files via upload

---
+++ numerical_methods/zad_01_02.m
@@ -0,0 +1,60 @@
+clear all;
+a = 0.0;
+b = pi;
+t0 = a;
+y0 = 1.0;
+
+f = @(t,y) -100*y + 100*cos(t) - sin(t);
+yd = @(t) cos(t);   % exact solution
+
+method = 'explicit;;;;lllllllllllllllllllllllllllllllllllllllllllll';
+% possible methods: 'explicit', 'implicit', 'trapezoidal', 'midpoint'
+
+for h = [0.4, 0.2, 0.1, 0.05, 0.01]
+    n = abs(a - b) / h;    % calculate number of steps based on step size and total time
+    t = linspace(a, b, n + 1);
+
+    ydd = zeros(round(n), 1);   % exact values at times k * h
+    for k = 1:n+1
+        ydd(k) = yd(t(k));
+    end
+ 
+    switch method
+        case 'explicit'
+            ya = [];
+            ya(1) = y0;
+            for k = 1:n
+                yar = ya(k) + h*f(t(k), ya(k));
+                ya = [ya, yar];
+            end
+
+        case 'implicit'
+            ya = [];
+            ya(1) = y0;
+            for k = 1:n
+                yar = (ya(k) + h * 100 * cos(t(k+1)) - h * sin(t(k+1))) / (1 + 100*h);
+                ya = [ya, yar];
+            end
+
+        case 'trapezoidal'
+            ya = [];
+            ya(1) = y0;
+            for k = 1:n
+                %yar = ;
+            end
+
+        case 'midpoint'
+            ya = [];
+            ya(1) = y0;
+            ya(2) = y0 + h * f(t0, y0);
+            for k=2:n
+                yar = ya(k-1) + 2 * h * ya(k);
+                ya = [ya, yar];
+            end
+    end
+
+    figure(h*1000);
+    plot(t, ya, '.', t, ydd, '-');
+    title("Grid: " + h);
+    ylim([-500, 500]);
+end
