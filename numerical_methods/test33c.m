1689be10dd96ac6faae86b9ab53896c10fe27c4f
Mateuszszszszsz <matisikorski2001@o2.pl>
2024-04-13 20:32:55 +0200

    NMIDE: 3_3c correct

--- numerical_methods/test33c.m
+++ numerical_methods/test33c.m
@@ -9,15 +9,16 @@ a = 0; b = 1;
 x = linspace(a, b, n+1);
 exact_solution = (x - 1) .* exp(x);
 
-rhs = @(x) -(x.^2 + 3) .* exp(x);
-
+g = @(x) -(x.^2 + 3) .* exp(x); %ok
+p = @(x) -(x+1);
+q = 2;
 A = zeros(n+1, n+1);
 b = zeros(n+1, 1);
 
 for i = 2:n
-    A(i, i+1) = -1+ (x(i)+1) * h / 2;
-    A(i, i) = 2 + 2 * h^2;
-    A(i, i-1) = -1 - (x(i+1)+1) * h / 2;
+    A(i, i+1) = -(1- p(x(i)) * h / 2);
+    A(i, i) = 2 + q * h^2;
+    A(i, i-1) = -(1+ p(x(i)) * h / 2);
 end
 
 A(1, 1) = 1;
@@ -26,7 +27,7 @@ b(1) = -1;
 b(n+1) = 0;
 
 for i = 2:n
-    b(i) = h^2 * rhs(x(i));
+    b(i) = h^2 * g(x(i));
 end
 
 y = A \ b;
