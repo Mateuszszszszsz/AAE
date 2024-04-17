a2183e22b52ecc70d1004d8a05f8204474de840d
Jakub <jakubmisiurka@gmail.com>
2024-04-17 16:04:13 +0200

    updated jacobi

--- numerical_methods_optimization/Lab5/landweber.m
+++ numerical_methods_optimization/Lab5/landweber.m
@@ -1,4 +1,4 @@
-function [x, residual_error, solution_error] = jacobi(A, b, max_iterations)
+function [x, residual_error, solution_error] = landweber(A, b, max_iterations, alpha)
     [~, n] = size(A);
     x = zeros(n, 1);
     
@@ -11,21 +11,22 @@ function [x, residual_error, solution_error] = jacobi(A, b, max_iterations)
 
     threshold = 10e-6;
 
-    s = diag(A);
-    S = diag(s);
-    T = S - A;
-    G = inv(S) * T;
+    S = (1/alpha) * eye(n);
+    T = S - A' * A;
+    G = T * alpha;
     sr = max(abs(eig(G)));
     
+    c = alpha * A' * b;
+
     i = 0;
 
     if sr > 1
-        disp('Spectral radius > 1, jacobi method will not converge');
+        disp('Spectral radius > 1, landweber method will not converge');
     else 
         while (i < max_iterations)
             i = i + 1;
             x_prev = x;
-            x = (T*x +  b) ./ s;
+            x = x_prev + alpha * A' * (b - A * x_prev);
 
             solution_error = [solution_error, norm(x - exact)/norm(exact)];
             residual_error = [residual_error, norm(A * x - b)/n];
