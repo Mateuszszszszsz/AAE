5d288852214b4d72653f4afcb442fe8a8ce3378a
Jakub <jakubmisiurka@gmail.com>
2024-04-22 20:27:27 +0200

    updated sor

--- numerical_methods_optimization/Lab5/successive_over_relaxation.m
+++ numerical_methods_optimization/Lab5/successive_over_relaxation.m
@@ -20,25 +20,25 @@ function [x, residual_error, solution_error] = successive_over_relaxation(A, b, 
     G = inv(S) * T;
     sr = max(abs(eig(G)));
 
-    i = 0;
+    iterations = 0;
 
     if sr > 1
         disp('Spectral radius > 1, sor method will not converge');
     else 
-        while (i < max_iterations)
-            i = i + 1;
+        while (iterations < max_iterations)
+            iterations = iterations + 1;
             x_prev = x;
             
-            for i=1:n
+                for i=1:n
                     sigma=0;
                     for j=1:i-1
-                        sigma=sigma + A(i, j) * x(j);
+                        sigma = sigma + A(i, j) * x(j);
                     end
         
                     for j=i+1:n
                         sigma = sigma + A(i, j) * x_prev(j);
                     end
-                    x(i)=(1/A(i,i))*(b(i)-sigma);
+                    x(i) = (1 - omega) * x_prev(i) + (omega / A(i, i)) * (b(i) - sigma);
                 end
 
             solution_error = [solution_error, norm(x - exact)/norm(exact)];
